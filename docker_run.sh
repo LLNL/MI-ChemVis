2a5d43dd3f4e6d7e327fc6329ad8f8a2193242bd
Peggy Li <li54@llnl.gov>
2019-01-27 20:34:12 -0800

    Changed docker, copy dir, not add, diff run command

--- docker_run.sh
+++ docker_run.sh
@@ -3,4 +3,4 @@
 # Run chemvis and show on port 5010
 port=5010
 #docker run --name=chemvis -v $(pwd):/code -p $port:5010 chemvis
-docker run --name=chemvis chemvis
+docker run --name=chemvis --rm  -it -p 5010:5010 chemvis
